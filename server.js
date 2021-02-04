42e093f5a885c62dd3d0f683552037a8948c2a0f
socratone <gim2origin@gmail.com>
2021-02-04 09:29:18 +0900

    First commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+var corsOptions = {
+  origin: ['http://127.0.0.1:5500'],
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
+
+app.delete('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log(`App listening at http://localhost:3000`);
+});
